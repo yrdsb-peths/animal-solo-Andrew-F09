d1acb4b004011337473550fd7435c2a2a28b5a62
Andrew-F09 <fionxia11@gmail.com>
2025-11-30 21:13:41 -0500

    Update Elephant.java

--- soloanimal/Elephant.java
+++ soloanimal/Elephant.java
@@ -15,12 +15,12 @@ public class Elephant extends Actor
     public void act()
     {
         //
-        if(Greenfoot.isKeyDown(("left"))
+        if(Greenfoot.isKeyDown("left"))
         {
             move(-1);
         }
         
-        else if (Greenfoot.isKeyDown(("right"))
+        else if (Greenfoot.isKeyDown("right"))
         {
             move(1);
         }
