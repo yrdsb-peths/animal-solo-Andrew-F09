e955672b329ce4addc653b8feb2567ac3d2e8a70
Andrew-F09 <fionxia11@gmail.com>
2025-11-30 22:40:26 -0500

    Update TitleScreen.java

--- soloanimal/TitleScreen.java
+++ soloanimal/TitleScreen.java
@@ -3,8 +3,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class TitleScreen here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author (fok) 
+ * @version 1.0
  */
 public class TitleScreen extends World
 {
